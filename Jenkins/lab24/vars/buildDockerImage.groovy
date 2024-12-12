eb5366be631612c6b28fc6868c64241b8d1420f8
Muhammad Gamal <59343923+mujemi26@users.noreply.github.com>
2024-12-12 21:16:20 +0200

    Update and rename pipelineConfig.groovy to buildDockerImage.groovy

---
+++ Jenkins/lab24/vars/buildDockerImage.groovy
@@ -0,0 +1,6 @@
+def call(String imageName, String buildNumber) {
+    sh """
+        echo "Building image: ${imageName}:${buildNumber}"
+        docker build -t ${imageName}:${buildNumber} .
+    """
+}
