30488b712afe0dd3bc7cfc46ad0d5c683a0aee77
Muhammad Gamal <59343923+mujemi26@users.noreply.github.com>
2024-12-12 21:17:25 +0200

    Create pushToDockerHub.groovy

---
+++ Jenkins/lab24/vars/pushToDockerHub.groovy
@@ -0,0 +1,10 @@
+def call(String imageName, String buildNumber, String credentialsId) {
+    withDockerRegistry([credentialsId: credentialsId]) {
+        sh """
+            echo "Pushing images to Docker Hub..."
+            docker push ${imageName}:${buildNumber}
+            docker tag ${imageName}:${buildNumber} ${imageName}:latest
+            docker push ${imageName}:latest
+        """
+    }
+}
